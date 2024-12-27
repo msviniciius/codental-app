7ded668f74f9e9f3900d4598ab17e18a94ad2e83
msviniciius <msvinicius.ads@gmail.com>
2024-12-27 20:29:29 +0000

    add: enable_starttls_auto: true em redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,7 +2,6 @@ require 'redis'
 
 $redis = Redis.new(
   url: ENV['REDIS_URL'],
-  enable_starttls_auto: true
+  enable_starttls_auto: true,
   ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
 )
-
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,15 +1,23 @@
-Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
-end
+if Rails.env.production?
+  Sidekiq.configure_server do |config|
+    config.redis = { 
+      url: ENV['REDIS_URL'], 
+      ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } 
+    }
+  end
 
-Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
-end
+  Sidekiq.configure_client do |config|
+    config.redis = { 
+      url: ENV['REDIS_URL'], 
+      ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } 
+    }
+  end
+else
+  Sidekiq.configure_server do |config|
+    config.redis = { url: 'redis://localhost:6379/0' }
+  end
 
-Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_URL'] }
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_URL'] }
+  Sidekiq.configure_client do |config|
+    config.redis = { url: 'redis://localhost:6379/0' }
+  end
 end
