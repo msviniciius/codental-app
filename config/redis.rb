dcce93cf49db859109c1aba7d7050761466cc787
msviniciius <msvinicius.ads@gmail.com>
2024-12-29 15:37:47 +0000

    correçoes no redis

--- config/redis.rb
+++ config/redis.rb
@@ -1,6 +1,17 @@
+require 'sidekiq'
+
 redis_url = ENV['REDIS_URL'] || 'redis://localhost:6379/1'
 
-Redis.new(
-  url: redis_url,
-  ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } # Use VERIFY_PEER se o certificado for confiável.
-)
+Sidekiq.configure_server do |config|
+  config.redis = {
+    url: redis_url,
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = {
+    url: redis_url,
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  }
+end
