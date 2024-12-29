212722c8144acd87b2caf656831cae687268224d
msviniciius <msvinicius.ads@gmail.com>
2024-12-29 17:03:33 +0000

    fix

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,16 +1,12 @@
 if Rails.env.production?
-  Sidekiq.configure_server do |config|
-    config.redis = { 
-      url: ENV['REDIS_URL'], 
-      ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } 
-    }
-  end
-
   Sidekiq.configure_client do |config|
-    config.redis = { 
-      url: ENV['REDIS_URL'], 
-      ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } 
-    }
+    config.redis = { url: ENV['REDIS_URL'], size: 1, network_timeout: 5, 
+      ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
+  end
+  
+  Sidekiq.configure_server do |config|
+    config.redis = { url: ENV['REDIS_URL'], size: 7, network_timeout: 5, 
+      ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
   end
 else
   Sidekiq.configure_server do |config|
--- config/redis.rb
+++ config/redis.rb
@@ -2,16 +2,9 @@ require 'sidekiq'
 
 redis_url = ENV['REDIS_URL'] || 'redis://localhost:6379/1'
 
-Sidekiq.configure_server do |config|
-  config.redis = {
-    url: redis_url,
-    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+$redis = Redis.new(
+  url: redis_url,
+  ssl_params: {
+    verify_mode: OpenSSL::SSL::VERIFY_NONE
   }
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = {
-    url: redis_url,
-    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
-  }
-end
+)
