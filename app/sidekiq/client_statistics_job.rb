b9d6257a4023675fd47659e940cf2bfe43bf510c
msviniciius <msvinicius.ads@gmail.com>
2024-12-29 19:28:20 +0000

    fix: melhorias e verificações ClientStatisticsJob

--- app/sidekiq/client_statistics_job.rb
+++ app/sidekiq/client_statistics_job.rb
@@ -3,9 +3,13 @@ class ClientStatisticsJob
 
   def perform
     total_clients = Client.count
+
     duplicate_phones = Client.group(:phone).having('count(phone) > 1').count
     duplicate_phones = duplicate_phones.is_a?(Hash) ? duplicate_phones : {}
-    
+
+    clients_by_state = Client.group(:state).count
+    clients_by_state = clients_by_state.is_a?(Hash) ? clients_by_state : {}
+
     sleep(1)
   
     # Envia os dados para o canal do ActionCable
