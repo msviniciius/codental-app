db52f609b5cd94045b76e8c926840b12c820a884
msviniciius <msvinicius.ads@gmail.com>
2024-12-29 19:24:14 +0000

    fix: ClientStatisticsJob

--- app/sidekiq/client_statistics_job.rb
+++ app/sidekiq/client_statistics_job.rb
@@ -4,16 +4,10 @@ class ClientStatisticsJob
   def perform
     total_clients = Client.count
     duplicate_phones = Client.group(:phone).having('count(phone) > 1').count
-    clients_by_state = Client.group(:state).count
-
-    # Adicione validações para garantir que os dados sejam do tipo esperado
-    Rails.logger.info "Duplicate Phones: #{duplicate_phones.inspect}"
-    Rails.logger.info "Clients by State: #{clients_by_state.inspect}"
-
-    # Se duplicate_phones ou clients_by_state forem vazios, certifique-se de que os dados são um hash
-    duplicate_phones = {} if duplicate_phones.nil? || !duplicate_phones.is_a?(Hash)
-    clients_by_state = {} if clients_by_state.nil? || !clients_by_state.is_a?(Hash)
-
+    duplicate_phones = duplicate_phones.is_a?(Hash) ? duplicate_phones : {}
+    
+    sleep(1)
+  
     # Envia os dados para o canal do ActionCable
     ActionCable.server.broadcast 'clients_statistics_channel', {
       total_clients: total_clients,
