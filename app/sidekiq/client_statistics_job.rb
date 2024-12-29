4b8e2e1eb7be2e14be2885d747a27dde5ddb36f1
msviniciius <msvinicius.ads@gmail.com>
2024-12-29 18:57:41 +0000

    fix debugger

--- app/sidekiq/client_statistics_job.rb
+++ app/sidekiq/client_statistics_job.rb
@@ -4,17 +4,17 @@ class ClientStatisticsJob
   def perform
     Rails.logger.info "Starting ClientStatisticsJob"
 
-    # Registra o total de clientes
     total_clients = Client.count
-    Rails.logger.info "Total Clients: #{total_clients}"
 
-    # Registra os telefones duplicados
+    # Verifica e loga o tipo de 'duplicate_phones'
     duplicate_phones = Client.group(:phone).having('count(phone) > 1').count
     Rails.logger.info "Duplicate Phones: #{duplicate_phones.inspect}"
+    Rails.logger.info "Type of duplicate_phones: #{duplicate_phones.class}"
 
-    # Registra os clientes por estado
+    # Verifica e loga o tipo de 'clients_by_state'
     clients_by_state = Client.group(:state).count
     Rails.logger.info "Clients by State: #{clients_by_state.inspect}"
+    Rails.logger.info "Type of clients_by_state: #{clients_by_state.class}"
 
     sleep(1)
 
