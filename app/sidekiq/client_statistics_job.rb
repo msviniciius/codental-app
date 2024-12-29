bbf224b1eb2b4ca7203b7dab5fc3f2b682dd12af
msviniciius <msvinicius.ads@gmail.com>
2024-12-29 19:04:50 +0000

    fix debugger

--- app/sidekiq/client_statistics_job.rb
+++ app/sidekiq/client_statistics_job.rb
@@ -2,21 +2,17 @@ class ClientStatisticsJob
   include Sidekiq::Job
 
   def perform
-    Rails.logger.info "Starting ClientStatisticsJob"
-
     total_clients = Client.count
-
-    # Verifica e loga o tipo de 'duplicate_phones'
     duplicate_phones = Client.group(:phone).having('count(phone) > 1').count
-    Rails.logger.info "Duplicate Phones: #{duplicate_phones.inspect}"
-    Rails.logger.info "Type of duplicate_phones: #{duplicate_phones.class}"
-
-    # Verifica e loga o tipo de 'clients_by_state'
     clients_by_state = Client.group(:state).count
-    Rails.logger.info "Clients by State: #{clients_by_state.inspect}"
-    Rails.logger.info "Type of clients_by_state: #{clients_by_state.class}"
 
-    sleep(1)
+    # Adicione validações para garantir que os dados sejam do tipo esperado
+    Rails.logger.info "Duplicate Phones: #{duplicate_phones.inspect}"
+    Rails.logger.info "Clients by State: #{clients_by_state.inspect}"
+
+    # Se duplicate_phones ou clients_by_state forem vazios, certifique-se de que os dados são um hash
+    duplicate_phones = {} if duplicate_phones.nil? || !duplicate_phones.is_a?(Hash)
+    clients_by_state = {} if clients_by_state.nil? || !clients_by_state.is_a?(Hash)
 
     # Envia os dados para o canal do ActionCable
     ActionCable.server.broadcast 'clients_statistics_channel', {
@@ -24,7 +20,5 @@ class ClientStatisticsJob
       duplicate_phones: duplicate_phones,
       clients_by_state: clients_by_state
     }
-
-    Rails.logger.info "ClientStatisticsJob finished"
   end
 end
