56eb583b4c54a460544cb2b4d7065ba3e5557f02
msviniciius <msvinicius.ads@gmail.com>
2024-12-29 18:49:15 +0000

    debugger ClientStatisticsJob

--- app/sidekiq/client_statistics_job.rb
+++ app/sidekiq/client_statistics_job.rb
@@ -2,17 +2,29 @@ class ClientStatisticsJob
   include Sidekiq::Job
 
   def perform
+    Rails.logger.info "Starting ClientStatisticsJob"
+
+    # Registra o total de clientes
     total_clients = Client.count
+    Rails.logger.info "Total Clients: #{total_clients}"
+
+    # Registra os telefones duplicados
     duplicate_phones = Client.group(:phone).having('count(phone) > 1').count
+    Rails.logger.info "Duplicate Phones: #{duplicate_phones.inspect}"
+
+    # Registra os clientes por estado
     clients_by_state = Client.group(:state).count
-    
+    Rails.logger.info "Clients by State: #{clients_by_state.inspect}"
+
     sleep(1)
-  
+
     # Envia os dados para o canal do ActionCable
     ActionCable.server.broadcast 'clients_statistics_channel', {
       total_clients: total_clients,
       duplicate_phones: duplicate_phones,
       clients_by_state: clients_by_state
     }
+
+    Rails.logger.info "ClientStatisticsJob finished"
   end
 end
